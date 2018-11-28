98b232fc5d6d89b9aa121d44bad3ba9c08239f9e
David Herrmann <dh.herrmann@gmail.com>
2018-11-28 13:25:23 +0100

    c-efi: document main header
    Add a short documentation to the main c-efi.h header.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/c-efi.h
+++ src/c-efi.h
@@ -3,7 +3,12 @@
 /**
  * UEFI Reference Specification Protocol Constants and Definitions
  *
- * XXX
+ * This project provides a basic UEFI environment as well as the definitions of
+ * protocol constants from the UEFI Specification. The c-efi-base.h header
+ * provides the basic programming environment that is necessary once you
+ * compile without a Standard C Library. The c-efi-system.h header provides the
+ * UEFI system integration including global vtables and types. All remaining
+ * functionality is provided in UEFI protocols.
  */
 
 #ifdef __cplusplus
